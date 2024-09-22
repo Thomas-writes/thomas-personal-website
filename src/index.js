e9a597fcfe838af62ccda2a5e8fce62bbbd2955b
Thomas Savasten <thomas@savasten.com>
2024-09-21 19:59:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { ChakraProvider  } from '@chakra-ui/react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render{
   <ChakraProvider>
@@ -11,7 +9,3 @@ ReactDOM.render{
   </ChakraProvider>
   document.getElementById('root')
 }
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
