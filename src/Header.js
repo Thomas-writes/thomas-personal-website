b8137bab02a6041f310ba8bbb781dce8b0aec065
Thomas Savasten <thomas@savasten.com>
2024-10-14 14:29:02 -0500

    Alignment still not right

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ import Navmenu from "./Navmenu";
 const Header = ({onSelectSection, color}) => {
     return (  
         <Box height="20vh" textAlign="center" py={10}>
-            <HStack spacing={'20px'} >
-                <Heading as="h1" fontSize="50px" color={color}>
+            <HStack spacing={'20px'} justify="center" width="100%" wrap="nowrap">
+                <Heading as="h1" fontSize="50px" color={color} whiteSpace="nowrap">
                     Thomas Savasten
                 </Heading>
                 <Navmenu color={color} onSelectSection={onSelectSection}/>
