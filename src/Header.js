cab3e6b4dc4e42769db8c5c0fcd12481bd9cdda6
Thomas Savasten <thomas@savasten.com>
2024-10-14 14:46:11 -0500

    ''

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = ({ onSelectSection, color }) => {
     return (
         <Box height="20vh" textAlign="center" py={10}>
             <Flex direction="row" justify="center" align="center" width="100%" wrap="nowrap" gap="20px">
-                <Heading as="h1" fontSize={["30px", "50px"]} color={color} whiteSpace="nowrap" letterSpacing="0.01em" lineHeight="1.1"> 
+                <Heading minWidth="300px" as="h1" fontSize={["30px", "50px"]} color={color} whiteSpace="nowrap"> 
                     Thomas Savasten
                 </Heading>
                 <Navmenu color={color} onSelectSection={onSelectSection} />
