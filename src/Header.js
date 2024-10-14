2e72b17ec1be6292ced3de5cedfb21de43f73e4b
Thomas Savasten <thomas@savasten.com>
2024-10-14 15:06:13 -0500

    that was not it lol

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ const Header = ({ onSelectSection, color }) => {
     return (
         <Box height="20vh" textAlign="center" py={10}>
             <HStack>
-                <Heading mr={4}as="h1" fontSize='50px' color={color} whiteSpace="nowrap" flexShrink={0} flexGrow={0} maxWidth="70%">
+                <Heading mr="2vw" as="h1" fontSize='50px' color={color} whiteSpace="nowrap" maxWidth="100%">
                     Thomas Savasten
                 </Heading>
                 <Navmenu color={color} onSelectSection={onSelectSection} />
