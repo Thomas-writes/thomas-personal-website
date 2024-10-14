efe508abdbd01f6bb134ba3340121798dbd9566d
Thomas Savasten <thomas@savasten.com>
2024-10-14 15:03:26 -0500

    if it was that easy imma cry

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,15 @@
-import { Heading, Box, Text, Flex } from "@chakra-ui/react";
+import { Heading, Box, Text, HStack } from "@chakra-ui/react";
 import Navmenu from "./Navmenu";
 
 const Header = ({ onSelectSection, color }) => {
     return (
-        <Box height="25vh" textAlign="center" py={10}> {/* Increased height */}
-            <Flex direction="row" justify="center" align="center" width="100%" wrap="nowrap" gap="20px"> {/* Reduced gap */}
-                <Heading as="h1" fontSize='50px' color={color} whiteSpace="nowrap" flexShrink={0} flexGrow={0} maxWidth="70%">
+        <Box height="20vh" textAlign="center" py={10}>
+            <HStack>
+                <Heading mr={4}as="h1" fontSize='50px' color={color} whiteSpace="nowrap" flexShrink={0} flexGrow={0} maxWidth="70%">
                     Thomas Savasten
                 </Heading>
                 <Navmenu color={color} onSelectSection={onSelectSection} />
-            </Flex>
+            </HStack>
             <Text as="b" fontSize="20px" mt={2}>
                 Welcome to my personal website
             </Text>
