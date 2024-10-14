bce326893e0f88dbc3d1c2b4ff1ddb6146753958
Thomas Savasten <thomas@savasten.com>
2024-10-14 14:58:24 -0500

    ''

--- src/Header.js
+++ src/Header.js
@@ -3,23 +3,9 @@ import Navmenu from "./Navmenu";
 
 const Header = ({ onSelectSection, color }) => {
     return (
-        <Box height="20vh" textAlign="center" py={10}>
-            <Flex 
-                direction={["column", "row"]}  // Stack on smaller screens, row on larger
-                justify="center" 
-                align="center" 
-                width="100%" 
-                wrap="wrap"  // Allow wrapping if there isn't enough space
-                gap="20px"
-            >
-                <Heading 
-                    as="h1" 
-                    fontSize={["30px", "50px"]} 
-                    color={color} 
-                    whiteSpace="nowrap"
-                    maxWidth="100%"  // Prevent heading from growing too wide
-                    textAlign="center" // Center the text to avoid alignment issues
-                >
+        <Box height="25vh" textAlign="center" py={10}> {/* Increased height */}
+            <Flex direction="row" justify="center" align="center" width="100%" wrap="nowrap" gap="20px"> {/* Reduced gap */}
+                <Heading as="h1" fontSize='50px' color={color} whiteSpace="nowrap" flexShrink={0} flexGrow={0} maxWidth="70%">
                     Thomas Savasten
                 </Heading>
                 <Navmenu color={color} onSelectSection={onSelectSection} />
