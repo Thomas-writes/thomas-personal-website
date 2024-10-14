0df3293a511a9208dedbab72edfb5dfd347676be
Thomas Savasten <thomas@savasten.com>
2024-10-14 14:32:55 -0500

    ''

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,20 @@
-import { Heading, Box, Text, HStack } from "@chakra-ui/react";
+import { Heading, Box, Text, Flex } from "@chakra-ui/react";
 import Navmenu from "./Navmenu";
 
-const Header = ({onSelectSection, color}) => {
-    return (  
+const Header = ({ onSelectSection, color }) => {
+    return (
         <Box height="20vh" textAlign="center" py={10}>
-            <HStack spacing={'20px'} justify="center" width="100%" wrap="nowrap">
-                <Heading as="h1" fontSize="50px" color={color} whiteSpace="nowrap">
+            <Flex direction="row" justify="center" align="center" width="100%" wrap="nowrap" gap="20px">
+                <Heading as="h1" fontSize={["30px", "50px"]} color={color} whiteSpace="nowrap">
                     Thomas Savasten
                 </Heading>
-                <Navmenu color={color} onSelectSection={onSelectSection}/>
-            </HStack>
-            <Text as ='b' fontSize ="20px" mt={2}>
+                <Navmenu color={color} onSelectSection={onSelectSection} />
+            </Flex>
+            <Text as="b" fontSize="20px" mt={2}>
                 Welcome to my personal website
             </Text>
         </Box>
     );
-}
- 
+};
+
 export default Header;
